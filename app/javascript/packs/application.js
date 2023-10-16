fa142f4ffb7a47edc1033983b3ffc29b2532800c
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2023-10-16 23:46:30 +0900

    いいねをjsonで

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,24 @@ require("channels")
 
 require("trix")
 require("@rails/actiontext")
+
+import $ from 'jquery'
+import axios from 'axios'
+
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $('.active-heart').removeClass('hidden')
+  } else {
+    $('.inactive-heart').removeClass('hidden')
+  }
+}
+
+document.addEventListener('turbolinks:load', () => {
+  const dataset = $('#article-show').data()
+  const articleId = dataset.articleId
+  axios.get(`/articles/${articleId}/like`)
+    .then((response) => {
+      const hasLiked = response.data.hasLiked
+      handleHeartDisplay(hasLiked)
+    })
+})
