5ed977b71417f546e3728a6427de71c981e28584
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2023-11-21 23:19:50 +0900

    factory botのインストールと修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,41 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  let!(:user) do
-    User.create!({
-      email: 'test@example.com', 
-      password: 'password'
-    })
-  end
+  let!(:user) { create(:user) }
 
   context 'タイトルと内容が入力されている場合' do
-    let!(:article) do
-      user.articles.build({
-        title: Faker::Lorem.characters(number: 10),
-        content: Faker::Lorem.characters(number: 300)
-      })
-    end
-    # before do
-    #   user = User.create!({
-    #     email: 'test@example.com', 
-    #     password: 'password'
-    #   })
-    #   @article = user.articles.build({
-    #     title: Faker::Lorem.characters(number: 10),
-    #     content: Faker::Lorem.characters(number: 300)
-    #   })
-    # end
+    let!(:article) { build(:article, user: user) }
+
     it '記事を保存できる' do
       expect(article).to be_valid
     end
   end
 
   context 'タイトルの文字が一文字の場合' do
-    let!(:article) do
-      user.articles.create({
-        title: Faker::Lorem.characters(number: 1),
-        content: Faker::Lorem.characters(number: 300)
-      })
+    let!(:article) { build(:article, title: Faker::Lorem.characters(number: 1), user: user) }
+
+    before do
+      article.save
     end
 
     it '記事を保存できない' do
