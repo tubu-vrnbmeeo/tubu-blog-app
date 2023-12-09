6e7e04cb69276ba2475bedc6d2e02934a5c16369
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2023-12-09 16:54:38 +0900

    chromedriverインストールし、試しに実行（未解決）

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,11 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+
+require 'selenium-webdriver'
+Selenium::WebDriver::Chrome.path = '/usr/bin/google-chrome'
+Webdrivers::Chromedriver.required_version = '120.0.6099.71'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
