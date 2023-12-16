24096df98c66719924c968d8d726f483913014ed
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2023-12-16 20:21:08 +0900

    修正（未解決、コンテンツ追加待ち）

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,9 +7,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-
-require 'selenium-webdriver'
-Selenium::WebDriver::Chrome.path = '/usr/bin/google-chrome'
 Webdrivers::Chromedriver.required_version = '120.0.6099.71'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
