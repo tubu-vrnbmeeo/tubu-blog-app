3f94f3cce12625a8985cbc0b690a3cbb51eeee02
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2024-01-16 10:10:25 +0900

    mailgun設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "blog_app_production"
 
   config.action_mailer.perform_caching = false
+  # 送信者のメールアドレスのドメイン
+  config.action_mailer.default_url_options = { host: 'example.com' }
+
+  # Mailgun
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => ENV['HEROKU_DOMAIN'],
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
